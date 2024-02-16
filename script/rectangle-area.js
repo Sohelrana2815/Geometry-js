e8bd1f152b7f71fe6e44eb564d19d61e137246cd
Sohelrana2815 <sohel152302@gmail.com>
2024-02-16 19:25:40 +0600

    Add calculate function

--- script/rectangle-area.js
+++ script/rectangle-area.js
@@ -1,3 +1,21 @@
 function calculateRectangleArea() {
-  console.log("Calculate Rectangle Area");
+  //get length of the rectangle
+  const widthInput = document.getElementById("rectangle-width");
+  const widthText = widthInput.value;
+  const width = parseFloat(widthText);
+  console.log(width);
+
+  //get length of the rectangle
+  const lengthInput = document.getElementById("rectangle-length");
+  const lengthText = lengthInput.value;
+  const length = parseFloat(lengthText);
+  console.log(length);
+
+  //Caculate rectangle area
+  const area = length * width;
+  console.log("Area of the rectangle is: ", area);
+
+  //display rectangle area
+  const rectangleAreaSpan = document.getElementById("rectangle-area");
+  rectangleAreaSpan.innerText = area;
 }
