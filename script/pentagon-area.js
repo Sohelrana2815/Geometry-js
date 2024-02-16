84122d3818d88abea933033a67113da55f5fc805
Sohelrana2815 <sohel152302@gmail.com>
2024-02-16 20:18:33 +0600

    Add calculate function ok

--- script/pentagon-area.js
+++ script/pentagon-area.js
@@ -1,7 +1,8 @@
 function calculatePentagonArea() {
   const perimeter = getInputValueById("pentagon-perimeter");
   const apothem = getInputValueById("pentagon-apothem");
-  console.log(perimeter, apothem);
+  const area = 0.5 * perimeter * apothem;
+  setInnerTextById("pentagon-area", area);
 }
 
 function getInputValueById(inputFieldId) {
@@ -9,4 +10,9 @@ function getInputValueById(inputFieldId) {
   const inputValueText = inputField.value;
   const value = parseFloat(inputValueText);
   return value;
+}
+
+function setInnerTextById(elementId, area) {
+  const element = document.getElementById(elementId);
+  element.innerText = area;
 }
